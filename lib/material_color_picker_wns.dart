be3870fa5aa3dcf85a5687f4ca3cc748bfdbc4f0
Ajmal Muhammad P <ajumalp@gmail.com>
2021-04-20 08:41:52 +0530

    Revert "fixed name issue"
    This reverts commit 7359582740d3bb3f5b3aafd8cb0178273b2104f7.

--- lib/material_color_picker_wns.dart
+++ lib/material_color_picker_wns.dart
@@ -1,5 +1,5 @@
 library material_color_picker_wns;
 
-export 'src/material_color_picker.dart' show MaterialColorPicker;
+export 'src/material_color_picker_wns.dart' show MaterialColorPicker;
 export 'src/circle_color.dart' show CircleColor;
 export 'src/colors.dart' show materialColors, accentColors, fullMaterialColors;
